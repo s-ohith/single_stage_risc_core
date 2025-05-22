b43ea327f870b2c43a84aab8bf72d162bee627b0
s-ohith <117926792+s-ohith@users.noreply.github.com>
2025-05-22 21:58:20 +0530

    Create mux.v

---
+++ mux.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.05.2025 20:17:09
+// Design Name: 
+// Module Name: mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mux (a,b,s,c);
+
+    input [31:0]a,b;
+    input s;
+    output [31:0]c;
+
+    assign c = (~s) ? a : b ;
+    
+endmodule
